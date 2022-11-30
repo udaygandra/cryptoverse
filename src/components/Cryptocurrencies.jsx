5740afe5b0705892f8ee25095ca20e3dbe48a24c
udaygandra <udaysaig25@gmail.com>
2022-11-29 22:45:20 -0500

    Added Search Functionality in Cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import {Link} from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
@@ -13,12 +13,25 @@ const Cryptocurrencies = ({simplified}) => {
 
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
 
+  const [searchTerm, setSearchTerm] = useState('')
+
+
+  useEffect(() =>{
+
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    setCryptos(filteredData);
+  
+  }, [cryptosList, searchTerm])
+
   console.log(cryptos);
 
 
   return (
     <>
-      {cryptos  && <Row gutter={[32, 32]} className="crypto-card-container">
+     {!simplified && <div className='search-crypto'>
+          <Input placeholder='search cryptocurrency...' onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div> }
+      <Row gutter={[32, 32]} className="crypto-card-container">
             {cryptos?.map((currency) => (
               <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.rank}> 
                 <Link to={`/crypto/${currency.id}`}>
@@ -30,7 +43,7 @@ const Cryptocurrencies = ({simplified}) => {
                 </Link>
               </Col>
             ))}
-       </Row>}
+       </Row>
     </>
   )
 }
