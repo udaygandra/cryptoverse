28868cd5b431be2db022de30fb679c760bed8b00
udaygandra <udaysaig25@gmail.com>
2022-12-26 22:33:13 -0500

    Toggle navbar button for mobile screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import {Link} from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined  } from '@ant-design/icons'
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenWidth, setScreenWidth] = useState(null);
+    
+    useEffect(() => {
+      const handleResize = () => setScreenWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      
+      handleResize()
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    useEffect(() => {
+      if (screenWidth < 768) {
+        setActiveMenu(false);
+      } else {
+        setActiveMenu(true);
+      }
+    }, [screenWidth]);
+
+
   return (
     <div className='nav-container'>
         <div className='logo-container'>
@@ -12,8 +33,11 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo">
                 <Link to="/">Cryptoverse</Link>
             </Typography.Title>    
+            <Button className='menu-control-container' onClick={()=> setActiveMenu(!activeMenu)}>
+                <MenuOutlined/>
+            </Button>
         </div>
-        <Menu theme="dark">
+       {activeMenu && <Menu theme="dark">
            <Menu.Item key="1" icon={<HomeOutlined/>}>
                 <Link to="/">Home</Link>
             </Menu.Item> 
@@ -26,7 +50,7 @@ const Navbar = () => {
             <Menu.Item key="4" icon={<BulbOutlined/>}>
                 <Link to="/news">News</Link>
             </Menu.Item>   
-        </Menu>
+        </Menu>}
     </div>
   )
 }
